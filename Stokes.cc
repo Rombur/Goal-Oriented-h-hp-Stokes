9d9126cd6f996291206d9ba6c419ee1e19d400c6
Arezou Ghesmati <ghesmati@gmail.com>
2016-12-09 17:39:29 -0600

    Add file  Stokes.cc

---
+++ Stokes.cc
@@ -0,0 +1,189 @@
+#include "StokesProblem.hh"
+#include "Parameters.hh"
+
+#include <fstream>
+#include <vector>
+
+using namespace dealii;
+
+
+void output_convergence(EXAMPLE example, REFINEMENT refinement, double theta,
+                        std::vector<unsigned int> const &n_dofs_per_cycle,
+                        std::vector<double> const &error_per_cycle)
+{
+  std::ofstream output_stream;
+  output_stream.open("convergence.txt");
+  switch (example)
+    {
+    case (example_1) :
+    {
+      output_stream<<"example_1 ";
+
+      break;
+    }
+    case (example_2) :
+    {
+      output_stream<<"example_2 ";
+
+      break;
+    }
+    case (example_3) :
+    {
+      output_stream<<"example_3 ";
+
+      break;
+    }
+    case (example_4) :
+    {
+      output_stream<<"example_4 ";
+
+      break;
+    }
+    case (example_5) :
+    {
+      output_stream<<"example_5 ";
+
+      break;
+    }
+    default :
+    {
+      AssertThrow(false, ExcMessage("Unknow Example"));
+    }
+    }
+
+  switch (refinement)
+    {
+    case (h_refine) :
+    {
+      output_stream<<"h_refinement ";
+
+      break;
+    }
+    case (p_refine) :
+    {
+      output_stream<<"p_refinement ";
+
+      break;
+    }
+    default :
+    {
+      output_stream<<"hp_refinement ";
+    }
+    }
+
+  output_stream<<theta<<std::endl;
+
+  for (unsigned int i=0; i<n_dofs_per_cycle.size(); ++i)
+    output_stream<<n_dofs_per_cycle[i]<<" "<<error_per_cycle[i]<<std::endl;
+
+  output_stream.close();
+}
+
+
+template <int dim>
+void run(Parameters const &parameters, StokesProblem<dim> &stokes_problem)
+{
+  std::vector<unsigned int> n_dofs_per_cycles;
+  std::vector<double> error_per_cycles;
+  if (parameters.do_goal_oriented())
+    std::cout<<"GOAL-ORIENTED"<<std::endl;
+  else
+    std::cout<<"HP-ADAPTIVITY"<<std::endl;
+
+  for (unsigned int cycle=0; cycle<=parameters.get_max_n_cycles(); ++cycle)
+    {
+      std::cout<< "-----------------------------------------------------------" << std::endl;
+      std::cout << "Cycle " << cycle << ':' << std::endl;
+      if (cycle == 0)
+        stokes_problem.generate_mesh();
+
+      std::cout<<"Number of active cells: "<< stokes_problem.n_active_cells() << std::endl;
+
+      std::cout<<"Set up"<<std::endl;
+      stokes_problem.setup_system(primal);
+
+      std::cout<<"Number of degrees of freedom: "<<stokes_problem.n_dofs()<<std::endl;
+      n_dofs_per_cycles.push_back(stokes_problem.n_dofs());
+
+      std::cout<<"Assemble system"<<std::endl;
+      stokes_problem.assemble_system(primal);
+
+      std::cout<<"Solve"<<std::endl;
+      stokes_problem.solve(primal);
+
+      std::cout<<"Compute the error"<<std::endl;
+      stokes_problem.compute_error();
+      std::cout<< "L2_norm of ERROR is: "<< stokes_problem.error_l2_norm() << std::endl;
+      error_per_cycles.push_back(stokes_problem.error_l2_norm());
+
+
+      std::cout<<"Compute the error estimate"<<std::endl;
+      stokes_problem.compute_error_estimator();
+      std::cout<< "L2_norm of ERROR Estimate is: " << stokes_problem.error_estimate_l2_norm()
+               << std::endl;
+
+      stokes_problem.compute_convergence_estimator(cycle);
+
+      if (parameters.do_goal_oriented())
+        {
+          std::cout<<"Set up dual problem"<<std::endl;
+          stokes_problem.setup_system(dual);
+
+          std::cout<<"Assemble dual system"<<std::endl;
+          stokes_problem.assemble_system(dual);
+
+          std::cout<<"Solve dual problem"<<std::endl;
+          stokes_problem.solve(dual);
+
+          std::cout<<"Compute the dual error"<<std::endl;
+          stokes_problem.compute_goal_error();
+
+          std::cout<<"Compute goal oriented error estimator"<<std::endl;
+          auto go_error_estimator_square = stokes_problem.compute_goal_oriented_error_estimator();
+
+          std::cout<<"Mark Cell"<<std::endl;
+          stokes_problem.mark_cells_goal_oriented(parameters.get_theta(),
+                                                  go_error_estimator_square);
+        }
+      else
+        {
+          std::cout<<"Mark Cell"<<std::endl;
+          stokes_problem.mark_cells(parameters.get_theta());
+        }
+
+      std::cout<<"Output results"<<std::endl;
+      stokes_problem.output_results(cycle);
+
+      std::cout<<"Refinement"<<std::endl;
+      stokes_problem.refine_in_h_p();
+
+      if (stokes_problem.error_estimate_l2_norm()<parameters.get_tolerance())
+        break;
+    }
+
+  output_convergence(parameters.get_example(), parameters.get_refinement(),
+                     parameters.get_theta(), n_dofs_per_cycles, error_per_cycles);
+}
+
+int main(int argc, char *argv[])
+{
+  // Silence a warning
+  (void) argc;
+  deallog.depth_console(0);
+  Parameters parameters(argv[1]);
+
+  ::MultithreadInfo::set_thread_limit();
+  if (parameters.get_dim()==2)
+    {
+      StokesProblem<2> stokes_problem(parameters);
+      run(parameters, stokes_problem);
+    }
+  else
+    {
+      StokesProblem<3> stokes_problem(parameters);
+      run(parameters, stokes_problem);
+    }
+
+
+  return 0;
+}
